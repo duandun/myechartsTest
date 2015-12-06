71bd98a7854db78b9dfef531c0339c291b5d306b
duandun <duandun@gmail.com>
2015-12-06 22:16:46 +0800

    gulp example

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require("gulp"),
-	uglify = require("gulp-uglify");
+	uglify = require("gulp-uglify"),
+	concat = require("gulp-concat");
 
 gulp.task("default", function() {
 
 });
 
 gulp.task('minify', function() {
-	
+	gulp.src(['js/jquery.js', 'js/testMinify.js'])
+		.pipe(uglify())
+		.pipe(concat('min.js'))
+		.pipe(gulp.dest('buld'))
 })
